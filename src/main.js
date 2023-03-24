68176048b5ba054b6b852776ea7b9af2ef75a229
osohyun0224 <osohyun0224@naver.com>
2023-03-24 16:15:50 +0900

    main.js service worker remove

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './registerServiceWorker'
+import './registerServiceWorker';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
