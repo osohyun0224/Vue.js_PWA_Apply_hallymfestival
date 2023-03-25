eb4ae9fe0b67f4131d73124367459d4e464c8b7b
osohyun0224 <osohyun0224@naver.com>
2023-03-25 14:50:30 +0900

    크롬 코드 수정

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,7 +1,7 @@
 import { register } from 'register-service-worker'
 
 /*if (process.env.NODE_ENV === 'production') { */
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  register(`/service-worker.js`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
@@ -27,4 +27,4 @@ import { register } from 'register-service-worker'
       console.error('Error during service worker registration:', error)
     }
   })
-/* }*
+/* }*/
